5d946a672762b8e1668883dcc5bd05ee474bc08a
Daniil Stepanov <Neoanalitik@gmail.com>
2022-01-31 21:32:15 +1000

    fixed namespace

--- laravel-api.ru/routes/api.php
+++ laravel-api.ru/routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\MeetingController;
-use App\Models\EmployeeController;
+use App\Http\Controllers\Api\EmployeeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
