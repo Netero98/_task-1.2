ade788b1b032cf4c44974539fc5b592ab8d804b7
Daniil Stepanov <Neoanalitik@gmail.com>
2022-01-31 20:20:19 +1000

    Сделал несколько методов в контроллере

--- laravel-api.ru/app/Http/Controllers/Api/MeetingController.php
+++ laravel-api.ru/app/Http/Controllers/Api/MeetingController.php
@@ -3,10 +3,55 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Meeting;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
-class MeetingController extends Controller
-{
+class MeetingController extends Controller {
+    
+     //Все собрания за промежуток времени
+    public function getMeetings(int $dayStarts,int $dayEnds) {
+        $meetings = DB::table('meetings')
+            ->where('startstamp', '>',date('Y-m-d H:i:s', $dayStarts))
+            ->where('endstamp', '<',date('Y-m-d H:i:s', $dayEnds))
+            ->orderBy('startstamp')
+            ->get();
+        return $meetings;
+    }
+
+    // оптимальное расписание за промежуток времени:
+    public function getOptimumMeetings($dayStarts, $dayEnds) {
+        $meetings = DB::table('meetings')
+            ->where('startstamp', '>',date('Y-m-d H:i:s', $dayStarts))
+            ->where('endstamp', '<',date('Y-m-d H:i:s', $dayEnds))
+            ->orderBy('startstamp')
+            ->get();
+                    
+        $meetingsArr = json_decode(json_encode($meetings), True); //неплохой способ сделать из объекта массив.
+        $optimumMeetingsArr = [];
+        while($meetingsArr) {
+            $idOfBestVar = 0;
+            $minimalDiff = INF;
+            foreach($meetingsArr as $rowId => $rowHimSelf) {
+                $newDiff = strtotime($rowHimSelf['startstamp']) - $dayStarts;
+                if($newDiff < 0) {
+                    unset($meetingsArr[$rowId]);
+                } elseif($newDiff < $minimalDiff) {
+                    $minimalDiff = $newDiff;
+                    $idOfBestVar = $rowId;
+                }
+            }
+            if($meetingsArr){
+            $optimumMeetingsArr[] = $meetingsArr[$idOfBestVar];
+            $dayStarts = strtotime($meetingsArr[$idOfBestVar]['endstamp']);
+            $dayStarts +=60; // следующее собрание должно начинаться хотя бы через минуту после конца предыдущего
+            unset($meetingsArr[$idOfBestVar]);
+            }
+        }
+        return $optimumMeetingsArr;
+    }
+
+
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +59,7 @@ class MeetingController extends Controller
      */
     public function index()
     {
-        //
+        return Meeting::all(); //на случай, если нужны все существующие расписания, а не за конкретный промежуток времени.
     }
 
     /**
@@ -36,7 +81,7 @@ class MeetingController extends Controller
      */
     public function show($id)
     {
-        //
+        return Meeting::find($id);
     }
 
     /**
