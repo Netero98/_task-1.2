4121c83c2c339b959060e26ec19476219af515e8
Daniil Stepanov <Neoanalitik@gmail.com>
2022-02-02 11:07:46 +1000

    Made tests for MeetingController

---
+++ laravel-api.ru/tests/Feature/MeetingControllerTest.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\Response;
+use Tests\TestCase;
+use App\Models\Meeting;
+
+class MeetingControllerTest extends TestCase {
+
+   public function testIndexReturnsDataInValidFormat() {
+      $this->json('get', 'api/meetings')
+         ->assertStatus(Response::HTTP_OK)
+         ->assertJsonStructure(
+            [
+               'data' => [
+                  '*' => [
+                     'id',
+                     'name',
+                     'startstamp',
+                     'endstamp',
+                     'created_at',
+                  ]
+               ]
+            ]
+      );
+   }
+
+   public function testMeetingIsCreatedSuccessfully() {
+      $randomStart = (1643580060 + rand(0,1000)*3600);
+      $startStamp = date('Y-m-d H:i:s', $randomStart);
+      $endStamp = date('Y-m-d H:i:s', $randomStart + 3000);
+      $infoToCreate = 
+         [
+            'name' => $this->faker->sentence(),
+            'startstamp' => $startStamp,
+            'endstamp' => $endStamp
+         ];
+      $this->json('post', 'api/meetings', $infoToCreate)
+         ->assertStatus(Response::HTTP_CREATED)
+         ->assertJsonStructure(
+            [
+               'data' => [
+                  'id',
+                  'name',
+                  'startstamp',
+                  'endstamp',
+                  'created_at',
+               ]
+            ]
+         );
+      $this->assertDatabaseHas('meetings', $infoToCreate);
+   }
+
+
+
+   public function testMeetingIsShownCorrectly() {
+      $randomStart = (1643580060 + rand(0,1000)*3600);
+      $startStamp = date('Y-m-d H:i:s', $randomStart);
+      $endStamp = date('Y-m-d H:i:s', $randomStart + 3000);
+
+      $meeting = Meeting::create(
+         [
+            'name' => $this->faker->sentence(),
+            'startstamp' =>  $startStamp,
+            'endstamp' => $endStamp,
+         ]
+      );
+         
+      $this->json('get', "api/meetings/$meeting->id")
+         ->assertStatus(Response::HTTP_OK)
+         ->assertExactJson(
+            [
+               'data' => 
+                  [
+                     'id' => $meeting->id,
+                     'name' => $meeting->name,
+                     'startstamp' => $meeting->startstamp,
+                     'endstamp' => $meeting->endstamp,
+                     'created_at' => $meeting->created_at,
+                  ]
+            ]
+         );
+   }
+
+   public function testMeetingIsDestroyed() {
+      $randomStart = (1643580060 + rand(0,1000)*3600);
+      $startStamp = date('Y-m-d H:i:s', $randomStart);
+      $endStamp = date('Y-m-d H:i:s', $randomStart + 3000);
+      $meetingData =
+         [
+            'name' => $this->faker->word,
+            'startstamp' => $startStamp,
+            'endstamp' => $endStamp
+         ];
+
+      $meeting = Meeting::create($meetingData);
+      
+      $this->json('delete', "api/meetings/$meeting->id")
+            ->assertNoContent();
+      $this->assertDatabaseMissing('meetings', $meetingData);
+   }
+
+   public function testUpdateMeetingReturnsCorrectData() {
+      $randomStart = (1643580060 + rand(0,1000)*3600);
+      $startStamp = date('Y-m-d H:i:s', $randomStart);
+      $endStamp = date('Y-m-d H:i:s', $randomStart + 3000);
+
+      $meeting = Meeting::create(
+            [
+               'name' => $this->faker->word,
+               'startstamp' => $startStamp,
+               'endstamp' => $endStamp
+            ]
+      );
+      
+      $randomStart = (1643580060 + rand(0,1000)*3600);
+      $startStamp = date('Y-m-d H:i:s', $randomStart);
+      $endStamp = date('Y-m-d H:i:s', $randomStart + 3000);
+
+      $newMeetingInfo = [
+            'name' => $this->faker->word,
+            'startstamp' => $startStamp,
+            'endstamp' => $endStamp
+      ];
+            
+      $this->json('put', "api/meetings/$meeting->id", $newMeetingInfo)
+            ->assertStatus(Response::HTTP_OK)
+            ->assertExactJson(
+               [
+                  'data' => [
+                        'id' => $meeting->id,
+                        'name' => $newMeetingInfo['name'],
+                        'startstamp' => $newMeetingInfo['startstamp'],
+                        'endstamp' => $newMeetingInfo['endstamp'],
+                        'created_at' => $meeting->created_at,
+                  ]
+               ]
+            );
+   }
+}
